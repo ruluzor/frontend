557843ea49fd65d3037a0018c9afacc9070ee288
rl-soadco <ruben.lucas@soadco.com>
2021-01-11 20:58:30 +0100

    Update dashboard to include constructionInfoFile

--- src/views/content/Dashboard.vue
+++ src/views/content/Dashboard.vue
@@ -1,12 +1,7 @@
 <template>
     <div>
         Dashboard 
-        <a @click="$router.push('/main/customers')">Ir a customers</a>
+        <a @click="$router.push('/main/customers')">Ir a customers</a><br>
+        <a @click="$router.push('/main/constructionInfoReader')">Ir a constructionInfo Reader</a>
     </div>
 </template>
-
-<script>
-export default {
-    
-}
-</script>
